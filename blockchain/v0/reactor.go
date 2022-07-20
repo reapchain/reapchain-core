7c177f7f480dc7c6335318310b98f47d9838b9b7
stompesi <stompesi@gmail.com>
2022-07-20 12:18:47 +0900

    Add log

--- blockchain/v0/reactor.go
+++ blockchain/v0/reactor.go
@@ -502,6 +502,10 @@ FOR_LOOP:
 				// get the hash without persisting the state
 				var err error
 
+				fmt.Println("sync - sync setting height", firstState.SettingSteeringMember.Height)
+				fmt.Println("sync - sync setting", firstState.SettingSteeringMember.SteeringMemberAddresses)
+				fmt.Println("sync - sync coordinatorPubKey", firstState.SettingSteeringMember.CoordinatorPubKey.Address())
+
 				state.SettingSteeringMember = firstState.SettingSteeringMember.Copy()
 				state.NextQrnSet = firstState.NextQrnSet.Copy()
 				state.NextVrfSet = firstState.NextVrfSet.Copy()
